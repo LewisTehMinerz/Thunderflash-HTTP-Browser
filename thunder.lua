595d442ac97f36db2ff284eb4a3a2ec0df301ca3
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-01-09 11:10:12 +0000

    Update thunder.lua

--- thunder.lua
+++ thunder.lua
@@ -5,3 +5,10 @@ function getRedirects()
   f.close()
   h.close()
 end
+function downloadUI()
+  h = http.get( "https://raw.githubusercontent.com/MultHub/LMNet-OS/master/src/apis/ui.lua" )
+  f = fs.open( "/thunderflash/ui" )
+  f.write( h.readAll() )
+  f.close()
+  h.close()
+end

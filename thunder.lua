be8520567cf3bb394314f715c35d760453d9f7db
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-01-09 11:03:59 +0000

    Update thunder.lua

--- thunder.lua
+++ thunder.lua
@@ -1,5 +1,5 @@
 function getRedirects()
-  h = http.get( "" )
+  h = http.get( "https://raw.githubusercontent.com/lewislovesgames/Thunderflash-HTTP-Browser/master/redirect.thunder" )
   f = fs.open( "/thunderflash/tmp/redirect.thunder" )
   f.write( h.readAll() )
   f.close()
